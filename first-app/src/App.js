ca3c0a83faee868d100b785a071adfccf7de08ab
whplug <whplug@gmail.com>
2023-09-26 21:05:20 +0200

    .

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,10 @@
 function App() {
-
+const name = "All"
 return <div>
-<h1>Hello World</h1>
-<h1>Hello World</h1>
-<h1>Hello World</h1>
-<h1>Hello World</h1>
+<h1>Hello {name}</h1>
+<h1>Hello {name}</h1>
+<h1>Hello {name}</h1>
+<h1>Hello {name}</h1>
 <h1>Hello World</h1>
 <h1>Hello World</h1>
 </div>;
